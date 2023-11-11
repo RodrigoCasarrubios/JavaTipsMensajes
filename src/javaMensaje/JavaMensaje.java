4359823b6227583d851b6d87acac6ff36d056909
Clase <Clase@DESKTOP-CN7RHLU>
2023-11-11 22:20:29 +0100

    Ejercicio Terminado

--- src/javaMensaje/JavaMensaje.java
+++ src/javaMensaje/JavaMensaje.java
@@ -3,8 +3,13 @@ package javaMensaje;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 import java.awt.BorderLayout;
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class JavaMensaje {
 
@@ -45,6 +50,40 @@ public class JavaMensaje {
 		
 		JPanel panel_Principal = new JPanel();
 		frmEmpleoremoto.getContentPane().add(panel_Principal, BorderLayout.CENTER);
+		panel_Principal.setLayout(new BorderLayout(0, 0));
+		
+		JButton btnDerecha = new JButton("Right Button");
+		btnDerecha.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.out.println("Hiciste click en el Right button");
+				
+				JOptionPane.showMessageDialog(null, "Declara siempre las variables ;)" +  "\n" + "Cuidado con lo que haces");
+			}
+		});
+		panel_Principal.add(btnDerecha, BorderLayout.EAST);
+		
+		JButton btnIzquierda = new JButton("Left Button");
+		btnIzquierda.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.out.println("Hiciste click en el Left button");
+				
+				JOptionPane.showMessageDialog(null, "Al crear un objeto crea constructores ;)" +  "\n" + "Crea uno vacio primero");
+			}
+		});
+		panel_Principal.add(btnIzquierda, BorderLayout.WEST);
+		
+		JButton btnMedio = new JButton("Mid button");
+		btnMedio.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.out.println("Hiciste click en el Mid button");
+				
+				JOptionPane.showMessageDialog(null, "Para ser mas ordeando haz metodos" +  "\n" + "Hazlos pero con cabeza");
+			}
+		});
+		panel_Principal.add(btnMedio, BorderLayout.CENTER);
 	}
 
 }
